f827beeb544979cfeaa57559c76d80c9335b6f4a
Takaskues63 <bosku.student@gmail.com>
2026-06-13 14:36:51 +0300

    Refactor name checking logic to improve accuracy and efficiency in name matching

--- Search engine helper.py
+++ Search engine helper.py
@@ -6,16 +6,27 @@ def check_in_list(user_input):
     possible_names = []
 
     for name in names:
-        same_letters = []
+        words, same_letters_in_name = [], []
+        words = name.split(' ')
+        name = name.lower()
 
-        for letter in user_input:
-            if letter in name.lower() and same_letters.count(letter) < name.count(letter):
-                same_letters.append(letter)
+        for word in words:
+            same_letters = []
+            word = word.lower()
 
-        if len(same_letters) / len(name) > 0.54:
-            possible_names.append(name)
+            for letter in user_input:
+                if letter.lower() in word and same_letters.count(letter) < word.count(letter):
+                    same_letters.append(letter)
+
+                    if same_letters_in_name.count(letter) < user_input.count(letter):
+                        same_letters_in_name.append(letter)
+
+            if len(same_letters) / len(word) >= 0.70 and name not in possible_names or len(same_letters_in_name) / len(name) >= 0.54 and name not in possible_names:
+                possible_names.append(name)
 
     print(possible_names)
+
+
 
 while True:
     user_input = input('Enter name: (x to exit) ')
